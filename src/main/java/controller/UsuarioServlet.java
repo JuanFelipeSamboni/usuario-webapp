f6bbe168d3d84cbcbc8289be2741c5ed06935c05
JuanFelipeSamboni <juanfelipesambonilopez@gmail.com>
2025-07-01 13:48:55 -0500

    Create UsuarioServlet.java

---
+++ src/main/java/controller/UsuarioServlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+@WebServlet("/UsuarioServlet")
+public class UsuarioServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nombre = request.getParameter("nombre");
+        String email = request.getParameter("email");
+        // Simulación de almacenamiento
+        request.setAttribute("nombre", nombre);
+        request.setAttribute("email", email);
+        RequestDispatcher rd = request.getRequestDispatcher("resultado.jsp");
+        rd.forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("registrar.html");
+    }
+}
